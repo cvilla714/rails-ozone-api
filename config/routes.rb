fad9d1bb9837fdb2dda1cc210258871c1ea58cd3
Cosmel Villalobos <cosmelvg@gmail.com>
2021-05-26 01:12:39 -0400

    created the routes for making a get request to zipcode and also when making a post request

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get 'home/index'
+  get 'home/zipcode'
   root 'home#index'
+
+  post 'zipcode' => 'home#zipcode'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
